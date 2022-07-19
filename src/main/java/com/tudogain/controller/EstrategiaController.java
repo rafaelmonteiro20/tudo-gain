9fd87dabe54035e60b761e96bcc29135c37ed4cc
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2022-07-19 18:47:18 -0300

    Editando estratégia

--- src/main/java/com/tudogain/controller/EstrategiaController.java
+++ src/main/java/com/tudogain/controller/EstrategiaController.java
@@ -5,12 +5,15 @@ import javax.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tudogain.model.Estrategia;
@@ -37,9 +40,17 @@ public class EstrategiaController {
     }
 
     @PostMapping
-    public ResponseEntity<Object> save(@Valid @RequestBody Estrategia estrategia) {
+    public ResponseEntity<Object> salvar(@Valid @RequestBody Estrategia estrategia) {
         estrategia = estrategiaService.salvar(estrategia);
         return ResponseEntity.created(RequestUtils.location(estrategia)).build();
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void editar(@PathVariable Long id, @Valid @RequestBody Estrategia estrategia) {
+        Estrategia estrategiaSalva = estrategiaService.buscarPorId(id);
+        estrategiaSalva.setDescricao(estrategia.getDescricao());
+        estrategiaService.salvar(estrategiaSalva);
     }
 
 }
