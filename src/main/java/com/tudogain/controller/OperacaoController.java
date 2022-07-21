8de892a313e5cd484a8aa10570d088a07127918e
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2022-07-21 19:20:28 -0300

    Buscando operação por id

--- src/main/java/com/tudogain/controller/OperacaoController.java
+++ src/main/java/com/tudogain/controller/OperacaoController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +25,11 @@ public class OperacaoController {
     public Page<Operacao> pesquisar(@PageableDefault(sort = "dataAbertura",
         direction = Direction.DESC) Pageable pageable) {
         return operacaoService.pesquisar(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public Operacao buscarPorId(@PathVariable Long id) {
+        return operacaoService.buscarPorId(id);
     }
 
 }
--- src/main/java/com/tudogain/service/OperacaoService.java
+++ src/main/java/com/tudogain/service/OperacaoService.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.tudogain.exception.ResourceNotFoundException;
 import com.tudogain.model.Operacao;
 import com.tudogain.repository.OperacaoRepository;
 
@@ -17,6 +18,11 @@ public class OperacaoService {
 
     public Page<Operacao> pesquisar(Pageable pageable) {
         return operacaoRepository.findAll(pageable);
+    }
+
+    public Operacao buscarPorId(Long id) {
+        return operacaoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Operação não encontrada."));
     }
 
 }
